0784d47629c22eca1070bfff840583e28424e928
beaumanvienna (JC) <beaumanvienna@gmail.com>
2021-04-24 13:45:02 -0700

    SDL added

--- scripts/build_sdl.sh
+++ scripts/build_sdl.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 echo "building sdl"
 cd vendor/sdl
+git update-index --assume-unchanged include/SDL_config.h include/SDL_revision.h
 ./configure
 make -j$(cat /proc/cpuinfo | grep -c vendor_id)

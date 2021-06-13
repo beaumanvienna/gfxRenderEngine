2d19ed295df1cedfc5ccf9ff5cc3c28ffa87cc95
beaumanvienna (JC) <beaumanvienna@gmail.com>
2021-06-13 08:25:34 -0700

    glfw3 build script removed

--- premake5.lua
+++ premake5.lua
@@ -78,7 +78,6 @@ project "engine"
     
     prebuildcommands
     {
-        "scripts/build_glfw.sh",
         "scripts/build_glew.sh",
         "scripts/build_sdl.sh",
         "scripts/build_sfml.sh",
@@ -99,3 +98,77 @@ project "engine"
         optimize "On"
 
 
+project "glfw3"
+	kind "StaticLib"
+	language "C"
+
+	targetdir ("vendor/glfw/build/src")
+	objdir ("vendor/glfw/build/objectFiles")
+
+	files
+	{
+		"vendor/glfw/include/GLFW/glfw3.h",
+		"vendor/glfw/include/GLFW/glfw3native.h",
+		"vendor/glfw/src/glfw_config.h",
+		"vendor/glfw/src/context.c",
+		"vendor/glfw/src/init.c",
+		"vendor/glfw/src/input.c",
+		"vendor/glfw/src/monitor.c",
+		"vendor/glfw/src/vulkan.c",
+		"vendor/glfw/src/window.c"
+	}
+	filter "system:linux"
+		pic "On"
+
+		systemversion "latest"
+		staticruntime "On"
+
+		files
+		{
+			"vendor/glfw/src/x11_init.c",
+			"vendor/glfw/src/x11_monitor.c",
+			"vendor/glfw/src/x11_window.c",
+			"vendor/glfw/src/xkb_unicode.c",
+			"vendor/glfw/src/posix_time.c",
+			"vendor/glfw/src/posix_thread.c",
+			"vendor/glfw/src/glx_context.c",
+			"vendor/glfw/src/egl_context.c",
+			"vendor/glfw/src/osmesa_context.c",
+			"vendor/glfw/src/linux_joystick.c"
+		}
+
+		defines
+		{
+			"_GLFW_X11"
+		}
+
+	filter "system:windows"
+		systemversion "latest"
+		staticruntime "On"
+
+		files
+		{
+			"vendor/glfw/src/win32_init.c",
+			"vendor/glfw/src/win32_joystick.c",
+			"vendor/glfw/src/win32_monitor.c",
+			"vendor/glfw/src/win32_time.c",
+			"vendor/glfw/src/win32_thread.c",
+			"vendor/glfw/src/win32_window.c",
+			"vendor/glfw/src/wgl_context.c",
+			"vendor/glfw/src/egl_context.c",
+			"vendor/glfw/src/osmesa_context.c"
+		}
+
+		defines 
+		{ 
+			"_GLFW_WIN32",
+			"_CRT_SECURE_NO_WARNINGS"
+		}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
